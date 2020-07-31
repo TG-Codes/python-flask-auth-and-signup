c1dbf7e699368491a2dc020bf2fb9a61d1e0ed6b
OrientzCoder <44888315+OrientzCoder@users.noreply.github.com>
2020-07-31 07:50:44 +0100

    commit

---
+++ project/main.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, Flask, render_template, redirect, url_for, flash 
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, login_required, current_user
+from .models import User
+from . import db
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', name=current_user.name)
+
